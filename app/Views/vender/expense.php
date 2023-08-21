dbc95cb4af45b2e4f1993fb14c00a1618e1f058a
Nikul <Nikul>
2023-08-21 11:35:07 +0530

    Changies

--- app/Views/vender/expense.php
+++ app/Views/vender/expense.php
@@ -3,36 +3,48 @@
         display: inline-flex;
         align-items: center;
         padding: 10px;
-        border-radius: 5px;
+        border-radius: 20px;
         background-color: #b5e7ee;
         color: white;
         cursor: pointer;
+    }
+
+    .txt-content-box {
+        border: 1px solid;
+    }
+
+    .txt-color-font {
+        color: #1414b7cf;
+    }
+
+    input[type=text] {
+        outline: none;
     }
 </style>
 
 <div class="container py-3">
     <div class="custom-container custom-center">
-    <h2 class="text-center"><u>Expenses</u></h2>
-    <div class="p-3 text-center">
-        <p>
-            Please Submit <br>
-            following document
-        </p>
-    </div>
-    <div class="border border-primary rounded ml-2 mr-5" style="background-color: #b5e7ee; width: 195px; height: 65px;">
-        <p class="text-center mt-1">
-            txt as entered by <br>
-            sender Ashok
-        </p>
-    </div>
-    <div class="mt-5">
-        <div class="d-flex align-items-center ml-3">
-            <input type="text" class="mr-3" style="width: 123px;height: 42px;">
-            <div class="rounded-icon">
-                <i class="fa fa-upload" aria-hidden="true"></i>
-            </div>
-            <p class="mb-0 ml-3"> <a href="<?php echo base_url('addmore') ?>">ADD FILE</a></p>
+        <h2 class="text-center"><u>Expenses</u></h2>
+        <div class="p-3 text-center">
+            <p>
+                Please Submit <br>
+                following document
+            </p>
         </div>
-    </div>
+        <div class="ml-2 mr-5 txt-content-box" style="background-color: #b5e7ee; width: 195px; height: 65px;">
+            <p class="text-center mt-1">
+                txt as entered by <br>
+                sender Ashok
+            </p>
+        </div>
+        <div class="mt-5">
+            <div class="d-flex align-items-center ml-3">
+                <input type="text" class="mr-3" style="width: 123px;height: 42px;">
+                <div>
+                    <a href="<?php echo base_url('addmore') ?>"><i class="fa fa-upload rounded-icon" aria-hidden="true"></i></a>
+                </div>
+                <p class="mb-0 ml-3"> <a href="<?php echo base_url('addmore') ?>" class="txt-color-font">ADD FILE</a></p>
+            </div>
+        </div>
     </div>
 </div>
--- app/Views/vender/index.php
+++ app/Views/vender/index.php
@@ -1,23 +1,29 @@
+<style>
+    input[type=text] {
+        outline: none;
+    }
+</style>
 <div class="container py-3">
     <div class="custom-center custom-container">
-    <h5 class="ml-0"><i class="fas fa-comments text-success"></i>&nbsp;&nbsp;<u>Notification Template</u></h5>
+        <h5 class="ml-0"><i class="fas fa-comments text-success"></i>&nbsp;&nbsp;<u>Notification Template</u></h5>
 
-    <div class="border border-dark pt-4 mt-4 v100">
-        <div>
-            <p class="ml-1 mb-0"><i class="far fa-envelope text-primary " style="font-size: 24px;"></i></p>
-            <div class="pl-3">
-                <p class="ml-4 mb-1">Dear Sandeep V</p>
-                <p class="ml-0 mb-1">Ashok from xyz Company</p>
-                <p class="ml-4 mb-1">Needs You to Submit</p>
-                <p class="ml-3 mb-1">following documents</p>
-                <p class="ml-0 mb-1">Pl click on the below link</p>
-                <p class="ml-5 mb-3"><a href="<?php echo base_url('expense') ?>" class="text-info">bitly.com</a></p>
+        <div class="border border-dark pt-4 mt-4 v100">
+            <div>
+                <p class="ml-1 mb-0"><i class="far fa-envelope text-primary " style="font-size: 24px;"></i></p>
+                <div class="pl-3 text-center">
+                    Dear Sandeep V <br>
+                    Ashok from xyz Company <br>
+                    Needs You to Submit <br>
+                    following documents <br>
+                    Pl click on the below link <br>
+                    <a href="<?php echo base_url('expense') ?>" class="text-info">bitly.com</a>
+                </div>
+
+            </div>
+            <div class="justify-content-between text-center mb-1 ml-2 mt-2">
+                <input type="text" class="mb-1" style="width: 180px;">&nbsp;
+                <i class="far fa-envelope text-primary" style="font-size: 24px;"></i>
             </div>
         </div>
-        <div class="justify-content-between align-items-center mb-2 ml-2">
-            <input type="text" class="ml-1 mb-1" style="width: 224px;">&nbsp;
-            <i class="far fa-envelope text-primary" style="font-size: 24px;"></i>
-        </div>
-    </div>
     </div>
 </div>
